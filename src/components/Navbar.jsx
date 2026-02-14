b7b26d054ed7accb5c4fbc6aebf9ca73352b699c
bipinThapax <bipin.thapax@gmail.com>
2026-02-14 22:00:33 +0545

    efficient Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,17 @@
 import { FaTools } from "react-icons/fa";
 import { IoIosSunny } from "react-icons/io";
 
+const navItems = ["Home", "Features", "Pricing", "Changelog", "Docs"];
+
+const navLinkClass =
+  "relative hover:text-white transition-all duration-500 ease-out " +
+  "after:absolute after:left-0 after:-bottom-px after:h-[2.5px] after:w-0 after:bg-[#114de5] " +
+  "cursor-pointer hover:after:w-full hover:after:transition-all hover:after:ease-in-out " +
+  "hover:after:duration-500 after:rounded active:after:w-[70%]";
+
 const Navbar = () => {
   return (
-    <nav className="px-10 py-6  z-30 flex flex-wrap justify-between items-center  font-semibold box-border bg-[#161515] sticky top-0">
+    <nav className="px-10 py-6 z-30 flex flex-wrap justify-between items-center font-semibold box-border bg-[#161515] sticky top-0">
       <div className="logo flex gap-2 items-center">
         <div className="logoImg text-[#1c4ecd]">
           <FaTools />
@@ -12,29 +20,19 @@ const Navbar = () => {
           <h1>DevVault</h1>
         </div>
       </div>
-      <ul className="list-none  ml-35 flex gap-10 items-center text-gray-500 ">
-        <li className="  relative  hover:text-white transition-all duration-500 ease-out after:absolute after:left-0 after:-bottom-px after:h-[2.5px] after:w-0 after:bg-[#114de5]  cursor-pointer hover:after:w-full hover:after:transition-all hover:after:ease-in-out hover:after:duration-500 after:rounded active:after:w-[70%]  ">
-          Home
-        </li>
-        <li className="  relative  hover:text-white transition-all duration-500 ease-out after:absolute after:left-0 after:-bottom-px after:h-[2.5px] after:w-0 after:bg-[#114de5]  cursor-pointer hover:after:w-full hover:after:transition-all hover:after:ease-in-out hover:after:duration-500 after:rounded active:after:w-[70%]  ">
-          Features
-        </li>
-        <li className=" relative  hover:text-white transition-all duration-500 ease-out after:absolute after:left-0 after:-bottom-px after:h-[2.5px] after:w-0 after:bg-[#114de5]  cursor-pointer hover:after:w-full hover:after:transition-all hover:after:ease-in-out hover:after:duration-500 after:rounded active:after:w-[70%] ">
-          Pricing
-        </li>
-        <li className=" relative  hover:text-white transition-all duration-500 ease-out after:absolute after:left-0 after:-bottom-px after:h-[2.5px] after:w-0 after:bg-[#114de5]  cursor-pointer hover:after:w-full hover:after:transition-all hover:after:ease-in-out hover:after:duration-500 after:rounded active:after:w-[70%] ">
-          Changelog
-        </li>
-        <li className=" relative  hover:text-white transition-all duration-500 ease-out after:absolute after:left-0 after:-bottom-px after:h-[2.5px] after:w-0 after:bg-[#114de5]  cursor-pointer hover:after:w-full hover:after:transition-all hover:after:ease-in-out hover:after:duration-500 after:rounded active:after:w-[70%] ">
-          Docs
-        </li>
+      <ul className="list-none ml-35 flex gap-10 items-center text-gray-500">
+        {navItems.map((item) => (
+          <li key={item} className={navLinkClass}>
+            {item}
+          </li>
+        ))}
       </ul>
       <div className="extra flex justify-center items-center gap-3">
         <div className="themetoggle cursor-pointer">
-          <IoIosSunny className="text-2xl hover:text-[#c1bbbb] transition-colors duration-300  " />
+          <IoIosSunny className="text-2xl hover:text-[#c1bbbb] transition-colors duration-300" />
         </div>
         <div className="login">
-          <button className=" px-4 py-2 rounded-xl text-sm font-medium cursor-pointer transition-all duration-300 ease-out active:scale-95 hover:-translate-y-px shadow-[0_0_0_1px_rgba(109,40,217,0.5)]">
+          <button className="px-4 py-2 rounded-xl text-sm font-medium cursor-pointer transition-all duration-300 ease-out active:scale-95 hover:-translate-y-px shadow-[0_0_0_1px_rgba(109,40,217,0.5)]">
             Log In
           </button>
         </div>
