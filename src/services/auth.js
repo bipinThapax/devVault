ef513006ce29b5bfdf40c174b3775774cdc8a668
bipinThapax <bipin.thapax@gmail.com>
2026-03-15 17:16:48 +0545

    auth api for frontend

---
+++ src/services/auth.js
@@ -0,0 +1,20 @@
+const API_URI = 'https://devevault-backend-6vdp.onrender.com/api/auth';
+
+export const registerUser = async (userData) => {
+    const response = await fetch(`${API_URI}/register`, {
+        method: 'POST',
+        headers: { 'Content-Type': "application/json" },
+        body: JSON.stringify(userData)
+    })
+    return response.json();
+}
+
+export const loginUser = async (credentials) => {
+    const response = await fetch(`${API_URI}/login`, {
+        method: 'POST',
+        headers: { 'Content-Type': "application.json" },
+        body: JSON.stringify(credentials)
+    })
+
+    return response.json();
+}
